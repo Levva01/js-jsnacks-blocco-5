2ac37b5c551719597641f00fc6db9cfeb637de7d
Levva01 <vellanifederico13@gmail.com>
2022-05-16 12:39:33 +0200

    correzione snack 1

--- snack-1/js/main.js
+++ snack-1/js/main.js
@@ -1,3 +1,13 @@
+/*
+    FUNCTIONS
+*/
+
+function rndNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+  
+
+
 class Zucchina {
     constructor(varieta, peso, lunghezza) {
         this.vavarieta =varieta;
@@ -10,7 +20,7 @@ let zucchine = [];
 let pesoTotale = 0;
 
 for(let i = 0; i < 10; i++){
-    let zucchina = new Zucchina(i, i*2, i+1);
+    let zucchina = new Zucchina(rndNumber(1, 5), rndNumber(1, 20), rndNumber(1, 30));
     zucchine.push(zucchina);
 }
 
